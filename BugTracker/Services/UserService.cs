a6911dfcb3e6d4e43eeb6a8d7c0768867adff391
JKrystofik <jkrystofik2290@hotmail.com>
2020-10-04 17:11:37 -0400

    added CURD database functions and database table printout for debugging.

--- BugTracker/Services/UserService.cs
+++ BugTracker/Services/UserService.cs
@@ -15,9 +15,54 @@ namespace BugTracker.Services
             _dbcontext = _db;
         }
 
-        public List<UsersClass> displayusers()
+        public List<UsersClass> GetAllUsers()
         {
             return _dbcontext.users.ToList();
+        }
+
+        public bool AddUser(UsersClass newuser)
+        {
+            _dbcontext.users.Add(newuser);
+            _dbcontext.SaveChanges();
+            return true;
+        }
+        public UsersClass EditUser(int user_id)
+        {
+            UsersClass uc = new UsersClass();
+            return _dbcontext.users.FirstOrDefault(u => u.id == user_id);
+        }
+
+        public bool UpdateUser(UsersClass user_update)
+        {
+            var Update = _dbcontext.users.FirstOrDefault(u => u.id == user_update.id);
+            if (Update != null)
+            {
+                Update.id = user_update.id;
+                Update.name = user_update.name;
+                Update.password = user_update.password;
+                Update.type = user_update.type;
+                _dbcontext.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteUser(UsersClass user_delete)
+        {
+            var Delete = _dbcontext.users.FirstOrDefault(u => u.id == user_delete.id);
+            if (Delete != null)
+            {
+                _dbcontext.Remove(Delete);
+                _dbcontext.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
